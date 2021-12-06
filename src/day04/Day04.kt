0daaaeecc8034eefd19098264b5c6ce23649cd35
eckerta <piIstGenau4>
2021-12-06 09:49:22 +0100

    fix day 04 after solving wrong year

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -1,50 +1,85 @@
-import java.lang.Exception
-
 fun main() {
     val testInput = readTestInput()
     val input = readInput()
 
-    check(part1(testInput) == 2)
+    check(part1(testInput) == 4512)
     println(part1(input))
 
-    check(part2(testInput) == 2)
+    check(part2(testInput) == 1924)
     println(part2(input))
 }
 
-private fun part1(input: List<String>, validator: (Map<String, String>) -> Boolean = { true }): Int {
-    val required = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid")
-    val joined = input.fold(mutableListOf("")) { acc, line ->
-        if (line.isBlank())
-            acc.also { acc += "" }
-        else
-            acc.also { acc[acc.lastIndex] += " $line" }
+private fun part1(input: List<String>): Int {
+    val boards = createBoards(input)
+
+    input[0].split(",").forEach {
+        val number = it.toInt()
+        for (board in boards) {
+            if (board.mark(number))
+                return board.sumOfUnmarked() * number
+        }
     }
-    val count = joined.map { pass ->
-        "${pass.trim()} cid:0"
-            .split(" ")
-            .map { it.split(":", limit = 2) }
-            .map { (k,v) -> k to v }
-            .toMap()
-    }.count { it.keys == required && validator(it.toSortedMap()) }
-    return count
+    return 0
 }
 
 private fun part2(input: List<String>): Int {
-    return part1(input) { map ->
-        val tests = listOf(
-            { map["byr"]!!.let { it.length == 4 && it.toInt() in 1920..2002 } },
-            { map["ecl"] in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth") },
-            { map["eyr"]!!.let { it.length == 4 && it.toInt() in 2020..2030 } },
-            { map["hcl"]!!.matches("#[0-9a-f]{6}".toRegex()) },
-            { map["hgt"]!!.let { (it.endsWith("cm") && it.dropLast(2).toInt() in 150..193) || (it.endsWith("in") && it.dropLast(2).toInt() in 59..76) } },
-            { map["iyr"]!!.let { it.length == 4 && it.toInt() in 2010..2020 } },
-            { map["pid"]!!.matches("[0-9]{9}".toRegex()) }
-        )
+    val boards = createBoards(input).toMutableList()
+    var lastScore = 0
 
-        try {
-            tests.all { it() }
-        } catch (e: Exception) {
-            false
+    input[0].split(",").forEach {
+        val number = it.toInt()
+        boards.removeIf { board ->
+            if (board.mark(number)) {
+                lastScore = board.sumOfUnmarked() * number
+                true
+            } else {
+                false
+            }
         }
+    }
+
+    return lastScore
+}
+
+private fun createBoards(input: List<String>): List<Board> {
+    return input.drop(2).windowed(6, 6, true) { field ->
+        val split = field.take(5).joinToString(" ").trim().split("\\s+".toRegex())
+        val numbers = split.map { it.toInt() }
+        Board(numbers)
+    }
+}
+
+private class Board(val numbers: List<Int>) {
+    companion object {
+        private val bingos = listOf(
+            0b00000_00000_00000_00000_11111,
+            0b00000_00000_00000_11111_00000,
+            0b00000_00000_11111_00000_00000,
+            0b00000_11111_00000_00000_00000,
+            0b11111_00000_00000_00000_00000,
+            0b00001_00001_00001_00001_00001,
+            0b00010_00010_00010_00010_00010,
+            0b00100_00100_00100_00100_00100,
+            0b01000_01000_01000_01000_01000,
+            0b10000_10000_10000_10000_10000
+        )
+    }
+
+    private var marked = 0
+
+    fun mark(number: Int) : Boolean {
+        val index = numbers.indexOf(number)
+        if (index >= 0) {
+            marked = marked or (1 shl index)
+        }
+        return isBingo()
+    }
+
+    fun isBingo(): Boolean = bingos.any { marked and it == it }
+
+    fun sumOfUnmarked() : Int {
+        return numbers.withIndex()
+            .filter { (index, number) -> marked and (1 shl index) == 0 }
+            .sumOf { it.value }
     }
 }
