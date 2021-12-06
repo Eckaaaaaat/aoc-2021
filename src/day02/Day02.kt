eebc1dbd6fff3d48b388183b96c5f4f28dee1c05
eckerta <piIstGenau4>
2021-12-06 08:40:30 +0100

    fix day 02 after solving wrong year

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -1,36 +1,38 @@
+import java.lang.IllegalArgumentException
+
 fun main() {
     val testInput = readTestInput()
     val input = readInput()
 
-    check(part1(testInput) == 2)
+    check(part1(testInput) == 150L)
     println(part1(input))
 
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 900L)
     println(part2(input))
 }
 
-private fun part1(input: List<String>): Int {
-    val regex = """(\d+)-(\d+) (.): (.*)""".toRegex()
-
-    fun String.isValid(): Boolean {
-        return regex.find(this)!!.let { match ->
-            val (_, from, to, letter, password) = match.groupValues
-            password.count { it == letter[0] } in from.toInt()..to.toInt()
-        }
-    }
-
-    return input.count { it.isValid() }
+private fun part1(input: List<String>): Long {
+    val grouped = input
+        .groupBy { it.split(" ")[0] }
+        .mapValues { (_, values) ->
+            values.map { it
+                .split(" ")[1].toLong() }
+                .sum() }
+    return grouped["forward"]!! * (grouped["down"]!! - grouped["up"]!!)
 }
 
-private fun part2(input: List<String>): Int {
-    val regex = """(\d+)-(\d+) (.): (.*)""".toRegex()
+private fun part2(input: List<String>): Long {
+    data class Pos(val a: Long, val h: Long, val d: Long)
 
-    fun String.isValid(): Boolean {
-        return regex.find(this)!!.let { match ->
-            val (_, first, second, letter, password) = match.groupValues
-            (password[first.toInt() - 1] == letter[0]) xor (password[second.toInt() - 1] == letter[0])
+    val pos = input.fold(Pos(0, 0, 0)) { acc, command ->
+        val (type, value) = command.split(" ").let { it[0] to it[1].toLong() }
+        when(type) {
+            "up" -> acc.copy(acc.a - value)
+            "down" -> acc.copy( acc.a + value)
+            "forward" -> acc.copy(h = acc.h + value, d = acc.d + acc.a * value)
+            else -> throw IllegalArgumentException()
         }
     }
 
-    return input.count { it.isValid() }
+    return pos.h * pos.d
 }
